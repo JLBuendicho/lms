0b23e78c41396ab14d78bf175442204cd28a376d
JLBuendicho <jlbuendichoschoolmail@gmail.com>
2026-05-01 11:06:38 +0800

    feat (db-seeder): add instructor assignment to student seeder

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -51,9 +52,12 @@ class UserFactory extends Factory
      */
     public function student(): static
     {
+        $instructors = User::where('role', 'instructor')->pluck('id')->toArray();
+
         return $this->state(fn (array $attributes) => [
             'role' => 'student',
             'lrn' => $this->faker->unique()->numerify('108263######'),
+            'assigned_instructor_id' => $this->faker->randomElement($instructors),
         ]);
     }
 

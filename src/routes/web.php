163f4fc65acdc05e88e12c0dc49589d087128f65
JLBuendicho <jlbuendichoschoolmail@gmail.com>
2026-05-14 20:40:31 +0800

    feat (dashboard): restricted access to dashboard to only student role users

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified', 'role:student'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
 
